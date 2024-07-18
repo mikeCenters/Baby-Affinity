1b62170326aa1193bc9433bdbb6a38d09a860b52
Michael Centers <michael.centers@icloud.com>
2024-07-17 23:54:36 -0400

    .

--- Baby Affinity/Baby_AffinityApp.swift
+++ Baby Affinity/Baby_AffinityApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct Baby_AffinityApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Name.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
