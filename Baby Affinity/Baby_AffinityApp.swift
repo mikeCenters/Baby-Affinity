b2a1d2240a56099698ced65d8ed08e80d0a0da38
Michael Centers <michael.centers@icloud.com>
2024-08-03 20:49:05 -0400

    Added a Model Container to have random favorite names.

--- Baby Affinity/Baby_AffinityApp.swift
+++ Baby Affinity/Baby_AffinityApp.swift
@@ -70,4 +70,43 @@ let previewModelContainer: ModelContainer = {
         fatalError("Could not create ModelContainer: \(error)")
     }
 }()
+
+
+/// `Model Container` used for testing and previews. Will have random favorites within the data.
+let previewModelContainer_WithFavorites: ModelContainer = {
+    let schema = Schema([
+        Name.self,
+    ])
+    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+    
+    do {
+        let container = try ModelContainer(for: schema, configurations: [modelConfiguration])
+        
+        /// Load preview data.
+        Task { @MainActor in
+            let context = container.mainContext
+            
+            let names = DefaultBabyNames()
+            
+            /// Add girl names.
+            for (sex, name) in names.girlNames {
+                let n = Name(name, sex: .female)
+                if n.text.contains("e") { n.toggleFavorite() }
+                context.insert(n)
+            }
+            
+            /// Add boy names.
+            for (sex, name) in names.boyNames {
+                let n = Name(name, sex: .male)
+                if n.text.contains("e") { n.toggleFavorite() }
+                context.insert(n)
+            }
+        }
+        
+        return container
+    } catch {
+        fatalError("Could not create ModelContainer: \(error)")
+    }
+}()
+
 #endif
