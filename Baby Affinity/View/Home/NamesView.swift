a14bdadefe4567e068ef8f10a538fdd5c12a7d40
Michael Centers <michael.centers@icloud.com>
2024-08-11 23:35:22 -0400

    Added all names view to show all names of a provided sex.

---
+++ Baby Affinity/View/Home/NamesView.swift
@@ -0,0 +1,18 @@
+//
+//  NamesView.swift
+//  Baby Affinity
+//
+//  Created by Mike Centers on 8/11/24.
+//
+
+import SwiftUI
+
+struct NamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NamesView()
+}
