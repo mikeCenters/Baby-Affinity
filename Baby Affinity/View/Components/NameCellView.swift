a6557d1f55a9f2e0b87f23faf9a063250f3f1234
Michael Centers <michael.centers@icloud.com>
2024-08-12 21:08:49 -0400

    Updated how the animation is triggered for the favorite icon.

--- Baby Affinity/View/Components/NameCellView.swift
+++ Baby Affinity/View/Components/NameCellView.swift
@@ -21,11 +21,11 @@ struct NameCellView: View {
     
     // MARK: - Controls and Constants
     
-    /// A boolean that represents the state of the favorite button press.
-    @State private var favIsTapped = false
-    
     /// Used to provide the maxWidth of the rank and favorites icon. This is used to place the name perfectly center within the cell.
     private let rankAndIconMaxWidth: CGFloat? = 60
+    
+    /// The value that represents the scale of the favorite icon.
+    @State private var imageScale: CGFloat = 1
     
     
     // MARK: - Body
@@ -55,14 +55,8 @@ struct NameCellView: View {
             
             Button {
                 /// Toggles the favorite status of the `Name` object.
-                withAnimation(.bouncy.speed(1.5)) {
+                withAnimation(.bouncy) {
                     name.toggleFavorite()
-                    favIsTapped.toggle()
-                    
-                } completion: {
-                    withAnimation(.bouncy.speed(1.5)) {
-                        favIsTapped.toggle()
-                    }
                 }
                 
             } label: {
@@ -71,11 +65,21 @@ struct NameCellView: View {
                     .font(.headline)
                     .foregroundColor(name.isFavorite ? .yellow : .gray)
                 /// Scale effect will upscale when tapped and is a favorite; otherwise, no scaling is perfomed.
-                    .scaleEffect(favIsTapped ? name.isFavorite ? 1.3 : 1 : 1)
+                    .scaleEffect(imageScale)
             }
             .frame(maxWidth: rankAndIconMaxWidth, alignment: .trailing)
             .sensoryFeedback(.impact, trigger: name.isFavorite)
             .buttonStyle(.borderless)   /// Disable List cell tapping.
+        }
+        .onChange(of: name.isFavorite) {
+            withAnimation(.bouncy) {
+                imageScale = name.isFavorite ? 1.5 : 1
+                
+            } completion: {
+                withAnimation(.bouncy) {
+                    imageScale = 1
+                }
+            }
         }
     }
 }
