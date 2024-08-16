de91a9900ba53fae0239d11fdd715a1188884056
Michael Centers <michael.centers@icloud.com>
2024-08-16 11:14:31 -0400

    Implemented the About View to give details of the app.

---
+++ Baby Affinity/View/Settings/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  Baby Affinity
+//
+//  Created by Mike Centers on 8/16/24.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
