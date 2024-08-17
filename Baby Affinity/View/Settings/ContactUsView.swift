3d04de1afd1b2f5f70ebef31941f45d7cad897c5
Michael Centers <michael.centers@icloud.com>
2024-08-17 00:50:23 -0400

    Updated the view to support the generic section view.

--- Baby Affinity/View/Settings/ContactUsView.swift
+++ Baby Affinity/View/Settings/ContactUsView.swift
@@ -15,37 +15,10 @@ import MessageUI
  */
 struct ContactUsView: View {
     
-    // MARK: - Properties
-    
-    /// The selected sex for which the names are filtered, stored in `AppStorage`.
-    @AppStorage("selectedSex") private var selectedSex = Sex.male
-    
-    
-    // MARK: Controls and Constants
-    
-    /// A Boolean value that indicates whether the device can send emails.
-    private var canSendEmails: Bool { MFMailComposeViewController.canSendMail() }
-    /// A state variable that controls the presentation of the mail compose view.
-    @State private var isShowingMailView: Bool = false
-    /// An array of email recipient addresses.
-    @State private var emailRecipients: [String] = []
-    /// The subject of the email.
-    @State private var emailSubject: String = ""
-    /// The body template of the email, including app version details.
-    @State private var emailBodyTemplate: String =
-        """
-        \n\n
-        =============
-        App Version: \(Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String ?? "Unable to get App Version.")
-        =============
-        """
-    
-    
     // MARK: - Body
     
     var body: some View {
         VStack(spacing: 0) {
-            
             HStack {
                 Spacer()
                 Image(systemName: "person.fill.questionmark")
@@ -56,126 +29,122 @@ struct ContactUsView: View {
                     .foregroundStyle(.primary, .tint)
                 Spacer()
             }
-            .padding(.bottom)
             
             ScrollView {
-                VStack(spacing: 0) {
-                    VStack(alignment: .leading, spacing: 16) {
-                        HStack {
-                            Spacer()
-                            Image(systemName: "questionmark.bubble")
-                                .headerSymbolStyle()
-                            Spacer()
-                        }
-                        
-                        Text("Need Help?")
-                            .font(.title).bold()
-                        
-                        Text("Contact support for any issues regarding the use of this app.")
-                        
-                        HStack {
-                            Spacer()
-                            Button {
-                                isShowingMailView.toggle()
-                                emailRecipients = ["support.babyaffinity@icloud.com"]
-                                emailSubject = "Support: "
-                            } label: {
-                                Text("Contact Support")
-                                    .bold()
-                            }
-                            .buttonStyle(.borderedProminent)
-                            Spacer()
-                        }
+                ForEach(Item.allCases, id: \.self) { item in
+                    SectionView(iconName: item.symbolName,
+                                title: item.title,
+                                description: item.bodyText) {
+                        item.extendedContent
                     }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                    
-                    Divider()
-                }
-                
-                VStack(spacing: 0) {
-                    VStack(alignment: .leading, spacing: 16) {
-                        HStack {
-                            Spacer()
-                            Image(systemName: "lightbulb.max")
-                                .headerSymbolStyle()
-                            Spacer()
-                        }
-                        
-                        Text("Feature Request")
-                            .font(.title).bold()
-                        
-                        Text("Help drive the direction of Baby Affinity by submitting a feature request.")
-                        
-                        HStack {
-                            Spacer()
-                            Button {
-                                isShowingMailView.toggle()
-                                emailRecipients = ["request.babyaffinity@icloud.com"]
-                                emailSubject = "Feature Request: "
-                            } label: {
-                                Text("Request Feature")
-                                    .bold()
-                            }
-                            .buttonStyle(.borderedProminent)
-                            Spacer()
-                        }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                    
-                    Divider()
-                }
-                
-                VStack(spacing: 0) {
-                    VStack(alignment: .leading, spacing: 16) {
-                        HStack {
-                            Spacer()
-                            Image(systemName: "note.text.badge.plus")
-                                .headerSymbolStyle()
-                            Spacer()
-                        }
-                        
-                        Text("Feedback")
-                            .font(.title).bold()
-                        
-                        Text("While the Baby Affinity app is tested heavily prior to release, errors with any app are possible. Let us know what we got right and wrong.")
-                        
-                        HStack {
-                            Spacer()
-                            Button {
-                                isShowingMailView.toggle()
-                                emailRecipients = ["feedback.babyaffinity@icloud.com"]
-                                emailSubject = "Feedback: "
-                            } label: {
-                                Text("Issue Feedback")
-                                    .bold()
-                            }
-                            .buttonStyle(.borderedProminent)
-                            Spacer()
-                        }
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding()
-                    
-                    Divider()
                 }
             }
         }
         .navigationTitle("Support")
-        .sheet(isPresented: $isShowingMailView) {
-            if canSendEmails {
-                MailView(isPresented: $isShowingMailView,
-                         recipients: $emailRecipients,
-                         subject: $emailSubject,
-                         body: $emailBodyTemplate)
-            } else {
-                EmailRequiredView()
+    }
+}
+
+
+// MARK: - Items
+
+extension ContactUsView {
+    /**
+     An enumeration representing the different types of contact options available in the `ContactUsView`.
+
+     - needHelp: Represents the option to contact support for assistance.
+     - featureRequest: Represents the option to submit a feature request.
+     - feedback: Represents the option to provide feedback about the app.
+     */
+    enum Item: Int, CaseIterable {
+        case needHelp, featureRequest, feedback
+        
+        /**
+         The name of the symbol to be used for the icon representing each contact option.
+         
+         - Returns: A `String` representing the symbol name.
+         */
+        var symbolName: String {
+            switch self {
+            case .needHelp:
+                return "questionmark.bubble"
+            case .featureRequest:
+                return "lightbulb.max"
+            case .feedback:
+                return "note.text.badge.plus"
+            }
+        }
+        
+        /**
+         The title to be displayed for each contact option.
+         
+         - Returns: A `String` representing the title.
+         */
+        var title: String {
+            switch self {
+            case .needHelp:
+                return "Need Help?"
+            case .featureRequest:
+                return "Feature Request"
+            case .feedback:
+                return "Feedback"
+            }
+        }
+        
+        /**
+         The descriptive text to be displayed for each contact option.
+         
+         - Returns: A `String` representing the body text.
+         */
+        var bodyText: String {
+            switch self {
+            case .needHelp:
+                return "Contact support for any issues regarding the use of this app."
+            case .featureRequest:
+                return "Help drive the direction of Baby Affinity by submitting a feature request."
+            case .feedback:
+                return "While the Baby Affinity app is tested heavily prior to release, errors with any app are possible. Let us know what we got right and wrong."
+            }
+        }
+        
+        /**
+         The extended content view to be displayed for each contact option.
+         
+         - Returns: A `View` representing the extended content.
+         */
+        var extendedContent: some View {
+            Group {
+                switch self {
+                case .needHelp:
+                    HStack {
+                        Spacer()
+                        SendEmailButton(emailRecipients: ["support.babyaffinity@icloud.com"],
+                                        emailSubject: "Support: ",
+                                        buttonText: "Contact Support")
+                        Spacer()
+                    }
+                    
+                case .featureRequest:
+                    HStack {
+                        Spacer()
+                        SendEmailButton(emailRecipients: ["request.babyaffinity@icloud.com"],
+                                        emailSubject: "Feature Request: ",
+                                        buttonText: "Request Feature")
+                        Spacer()
+                    }
+                    
+                case .feedback:
+                    HStack {
+                        Spacer()
+                        SendEmailButton(emailRecipients: ["feedback.babyaffinity@icloud.com"],
+                                        emailSubject: "Feedback: ",
+                                        buttonText: "Issue Feedback")
+                        Spacer()
+                    }
+                }
             }
         }
     }
 }
-
 
 #if DEBUG
 
