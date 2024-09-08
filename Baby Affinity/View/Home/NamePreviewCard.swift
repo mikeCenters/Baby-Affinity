c4519e8e379f1a49967b76449ccc9294c3708d5a
Mike Centers <michael.centers@icloud.com>
2024-09-08 13:28:24 -0400

    Refactored view. Set the lastName property to be read only.

--- Baby Affinity/View/Home/NamePreviewCard.swift
+++ Baby Affinity/View/Home/NamePreviewCard.swift
@@ -27,7 +27,7 @@ struct NamePreviewCard: View, NamePersistenceController {
     @State private var name: Name?
     
     /// The last name, stored in `AppStorage`.
-    @AppStorage("lastName") private var lastName = ""
+    @AppStorage("lastName") private(set) var lastName = ""
     
     
     // MARK: - Controls and Constants
@@ -54,73 +54,52 @@ struct NamePreviewCard: View, NamePersistenceController {
     
     var body: some View {
         VStack(spacing: 32) {
-            HStack {
-                Text("How's this name?")
-                    .font(.headline).bold()
-                Spacer()
-            }
+            
+            // MARK: - Title
+            
+            Text("How's this name?")
+                .font(.headline).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            
+            // MARK: - Name and Rating Buttons
             
             HStack {
                 getRatingButton(.decrease)
                     .font(.headline)
                 
-                Spacer()
-                Text(name?.text ?? "No")
-                Text(lastName)
-                Spacer()
+                Text("\(name?.text ?? "") \(lastName)")
+                    .font(.title2).bold()
+                    .frame(maxWidth: .infinity, alignment: .center)
                 
                 getRatingButton(.increase)
                     .font(.headline)
             }
-            .font(.title2).bold()
         }
         .listRowBackground(Color.accentColor.opacity(0.3))
-        // MARK: - On Appear
+        
+        
+        // MARK: - Task
+        
         .task {
             fetchAndStartTimer()
         }
     }
+}
+
+
+// MARK: - View Component
+
+extension NamePreviewCard {
     
+    // MARK: - Rating Button
     
-    // MARK: - View Component
-    
+    /// A button used to make an adjustment to the affinity rating of the `Name`.
+    /// - Parameter adjustment: The type that determines an increase or decrease in the rating.
+    /// - Returns: The `Button` view used to increase or decrease the rating.
     private func getRatingButton(_ adjustment: Adjustment) -> some View {
         return Button {
-            // Prevent button from being tapped repeatedly
-            if buttonsDisabled {
-                return
-            } else {
-                buttonsDisabled = true
-            }
-            
-            // Update the affinity rating
-            updateRating(adjustment)
-            
-            // Toggle image and show stars
-            withAnimation {
-                switch adjustment {
-                case .decrease:
-                    toggleImage(.decrease)
-                    dislikeImageShowStars = true
-                    
-                case .increase:
-                    toggleImage(.increase)
-                    likeImageShowStars = true
-                }
-            }
-            
-            // Hide stars after a short delay
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                withAnimation {
-                    switch adjustment {
-                    case .decrease:
-                        dislikeImageShowStars = false
-                        
-                    case .increase:
-                        likeImageShowStars = false
-                    }
-                }
-            }
+            ratingTapped(adjustment)
             
         } label: {
             Image(systemName: adjustment == .decrease ? dislikeImageName : likeImageName)
@@ -131,7 +110,7 @@ struct NamePreviewCard: View, NamePersistenceController {
         .overlay(
             ZStack {
                 if adjustment == .decrease ? dislikeImageShowStars : likeImageShowStars {
-                    ForEach(0..<5) { i in
+                    ForEach(0..<7) { i in
                         Image(systemName: "star.fill")
                             .foregroundColor(.yellow)
                             .scaleEffect(0.5)
@@ -146,6 +125,10 @@ struct NamePreviewCard: View, NamePersistenceController {
                 }
             }
         )
+        
+        
+        // MARK: - On Change
+        
         .onChange(of: adjustment == .decrease ? dislikeImageName : likeImageName ) {
             handleImageSizeChange(for: adjustment)
         }
@@ -153,33 +136,79 @@ struct NamePreviewCard: View, NamePersistenceController {
     }
     
     
-    // MARK: - Methods
+    // MARK: - Rating Methods
     
-    private func fetchAndStartTimer() {
-        if nameChangeTimer != nil { return }
+    private func ratingTapped(_ adjustment: Adjustment) {
+        // Prevent the buttons from being tapped repeatedly
+        if buttonsDisabled { return } else {
+            buttonsDisabled = true
+        }
         
-        fetchName()
+        // Update the affinity rating
+        updateRating(adjustment)
         
-        nameChangeTimer = Timer.scheduledTimer(withTimeInterval: rateOfNameChange, repeats: true) { _ in
+        handleRatingAnimitation(adjustment)
+    }
+    
+    private func updateRating(_ adjustment: Adjustment) {
+        guard let name = name else {
+            logError("Unable to get Name object in Name Preview Card")
+            return
+        }
+        switch adjustment {
+        case .decrease:
+            decreaseRating(of: name)
+            
+        case .increase:
+            increaseRating(of: name)
+        }
+    }
+    
+    private func increaseRating(of name: Name, by amount: Int = 50) {
+        do {
+            try name.setAffinity(name.affinityRating + amount)
+            
+        } catch {
+            logError("Unable to set affinity to new rating in Name Preview Card while increasing: \(error.localizedDescription)")
+        }
+    }
+    
+    private func decreaseRating(of name: Name, by amount: Int = 50) {
+        do {
+            try name.setAffinity(name.affinityRating - amount)
+            
+        } catch {
+            logError("Unable to set affinity to new rating in Name Preview Card while increasing: \(error.localizedDescription)")
+        }
+    }
+    
+    private func handleRatingAnimitation(_ adjustment: Adjustment) {
+        // Toggle image and show stars
+        withAnimation {
+            switch adjustment {
+            case .decrease:
+                toggleImage(.decrease)
+                dislikeImageShowStars = true
+                
+            case .increase:
+                toggleImage(.increase)
+                likeImageShowStars = true
+            }
+        }
+        
+        // Hide stars after a short delay
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             withAnimation {
-                fetchName()
-                setImagesInactive()
+                switch adjustment {
+                case .decrease:
+                    dislikeImageShowStars = false
+                    
+                case .increase:
+                    likeImageShowStars = false
+                }
             }
         }
     }
-    
-    private func fetchName() {
-        do {
-            name = try fetchNames(selectedSex).randomElement()
-            buttonsDisabled = false
-            
-        } catch {
-            logError("Unable to fetch names to generate a random name for Name Preview Card: \(error.localizedDescription)")
-        }
-    }
-    
-    
-    // MARK: - Image Methods
     
     private func handleImageSizeChange(for adjustment: Adjustment) {
         let temporarySize = CGSize(width: 1.3, height: 1.3)
@@ -234,39 +263,33 @@ struct NamePreviewCard: View, NamePersistenceController {
     private func toggleLikeImage() {
         likeImageName = likeImageName == likeInactiveImageName ? likeActiveImageName : likeInactiveImageName
     }
+}
+
+
+// MARK: - Methods
+
+extension NamePreviewCard {
     
-    
-    // MARK: - Rating Methods
-    
-    private func updateRating(_ adjustment: Adjustment) {
-        guard let name = name else {
-            logError("Unable to get Name object in Name Preview Card")
-            return
-        }
-        switch adjustment {
-        case .decrease:
-            decreaseRating(of: name)
-            
-        case .increase:
-            increaseRating(of: name)
-        }
-    }
-    
-    private func increaseRating(of name: Name, by amount: Int = 50) {
-        do {
-            try name.setAffinity(name.affinityRating + amount)
-            
-        } catch {
-            logError("Unable to set affinity to new rating in Name Preview Card while increasing: \(error.localizedDescription)")
+    private func fetchAndStartTimer() {
+        if nameChangeTimer != nil { return }
+        
+        fetchName()
+        
+        nameChangeTimer = Timer.scheduledTimer(withTimeInterval: rateOfNameChange, repeats: true) { _ in
+            withAnimation {
+                fetchName()
+                setImagesInactive()
+            }
         }
     }
     
-    private func decreaseRating(of name: Name, by amount: Int = 50) {
+    private func fetchName() {
         do {
-            try name.setAffinity(name.affinityRating - amount)
+            name = try fetchNames(selectedSex).randomElement()
+            buttonsDisabled = false
             
         } catch {
-            logError("Unable to set affinity to new rating in Name Preview Card while increasing: \(error.localizedDescription)")
+            logError("Unable to fetch names to generate a random name for Name Preview Card: \(error.localizedDescription)")
         }
     }
 }
