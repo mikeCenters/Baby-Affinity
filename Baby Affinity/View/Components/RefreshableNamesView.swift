dcf0f980bb25fef5dbcb479321c41bba90ffe6e5
Mike Centers <michael.centers@icloud.com>
2024-09-13 03:01:08 -0400

    .

---
+++ Baby Affinity/View/Components/RefreshableNamesView.swift
@@ -0,0 +1,18 @@
+//
+//  RefreshableNamesView.swift
+//  Baby Affinity
+//
+//  Created by Mike Centers on 9/13/24.
+//
+
+import SwiftUI
+
+struct RefreshableNamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RefreshableNamesView()
+}
